e5deb020b4b65391e4d552ab61ca394d1e906510
SwDarkCrow <132205783+SwDarkCrow@users.noreply.github.com>
2023-08-11 18:40:09 +0200

    Add responsive grid

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createGrid(number = 16) {
   for (let i = 0; i < number; i++) {
     for (let j = 0; j< number; j++) {
       let square = document.createElement('div');
-      square.style.minWidth = `${500 / number}px`;
+      square.style.minWidth = `${80 / number}vh`;
       container.appendChild(square);
     }
   }
