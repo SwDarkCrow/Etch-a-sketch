8d9c187b8eeeb481a2f0dfc852b35dbaa1658696
SwDarkCrow <132205783+SwDarkCrow@users.noreply.github.com>
2023-08-12 18:34:08 +0200

    Add redraw grid button

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ function createGrid(number = 16) {
       container.appendChild(square);
     }
   }
+  let squares = container.childNodes;
+  for (const square of squares){
+    square.addEventListener('mouseover', color);
+  }
 }
 
-createGrid(50);
-
-let squares = container.childNodes;
-for (const square of squares){
-  square.addEventListener('mouseover', color);
-}
+createGrid();
 
 function color(event){
   event.target.style.backgroundColor = `Black`;
 }
 
+function redraw(){
+  console.log('done');
+  let number = prompt('How big should the grid be?(max 100)');
+  if (number > 100) number = 100;
+  if (number === null) return;
+  var child = container.lastElementChild; 
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+  createGrid(number);
+}
