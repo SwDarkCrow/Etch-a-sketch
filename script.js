5b37e624a3cc1b3d30d2f360d6e23cd89599359b
SwDarkCrow <132205783+SwDarkCrow@users.noreply.github.com>
2023-08-12 21:48:20 +0200

    Add clear button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let container = document.querySelector('.container');
+let currentRows = 16;
 
 function createGrid(number = 16) {
   for (let i = 0; i < number; i++) {
@@ -12,6 +13,7 @@ function createGrid(number = 16) {
   for (const square of squares){
     square.addEventListener('mouseover', color);
   }
+  currentRows = number;
 }
 
 createGrid();
@@ -24,11 +26,16 @@ function randomColor(){
   return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 
-function redraw(){
-  console.log();
+let buttons = Array.from(document.querySelectorAll('button'));
+buttons[0].addEventListener('click', () => {
   let number = prompt('How big should the grid be?(max 100)');
   if (number > 100) number = 100;
   if (number === null) return;
+  redraw(number);
+})
+buttons[1].addEventListener('click', () => {redraw()});
+
+function redraw(number = currentRows){
   var child = container.lastElementChild; 
   while (child) {
     container.removeChild(child);
