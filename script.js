41ae36781dccb52f1ab370caae82c2dd4feadadc
SwDarkCrow <132205783+SwDarkCrow@users.noreply.github.com>
2023-08-12 21:33:21 +0200

    Add random color instead of black

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createGrid(number = 16) {
   for (let i = 0; i < number; i++) {
     for (let j = 0; j< number; j++) {
       let square = document.createElement('div');
-      square.style.minWidth = `${900 / number}px`;
+      square.style.minWidth = `${700 / number}px`;
       container.appendChild(square);
     }
   }
@@ -17,11 +17,15 @@ function createGrid(number = 16) {
 createGrid();
 
 function color(event){
-  event.target.style.backgroundColor = `Black`;
+  event.target.style.backgroundColor = randomColor();
+}
+
+function randomColor(){
+  return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 
 function redraw(){
-  console.log('done');
+  console.log();
   let number = prompt('How big should the grid be?(max 100)');
   if (number > 100) number = 100;
   if (number === null) return;
